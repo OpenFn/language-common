13928bca16aa8ff4dbb7cedef8e70876587b79ea
Stuart Corbishley <corbish@gmail.com>
2016-02-01 16:47:19 +0200

    Moved common source helpers from language-salesforce

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { curry, merge, reduce, zipObject } from 'lodash-fp';
+import JSONPath from 'JSONPath';
+
 /**
  * Execute a sequence of operations.
  * Main outer API for executing expressions.
@@ -21,3 +24,158 @@ export function execute(...operations) {
 
   }
 }
+
+/**
+ * Picks out a single value from source data.
+ * If a JSONPath returns more than one value for the reference, the first
+ * item will be returned.
+ * @constructor
+ * @param {String} path - JSONPath referencing a point in `state`.
+ * @param {State} state - Runtime state.
+ * @returns {String}
+ */
+export const sourceValue = curry(function(path, state) {
+  return JSONPath.eval(state, path)[0];
+});
+
+/**
+ * Picks out a value from source data.
+ * Will return whatever JSONPath returns, which will always be an array.
+ * If you need a single value use `sourceValue` instead.
+ * @constructor
+ * @param {string} path - JSONPath referencing a point in `state`.
+ * @param {State} state - Runtime state.
+ * @returns {Array.<String|Object>}
+ */
+export function source(path) {
+  return (state) => {
+    return JSONPath.eval(state, path)
+  }
+}
+
+/**
+ * Scopes an array of data based on a JSONPath.
+ * Useful when the source data has `n` items you would like to map to
+ * an operation.
+ * The operation will receive a slice of the data based of each item
+ * of the JSONPath provided.
+ * @example <caption>Simple Map</caption>
+ * map("$.[*]",
+ *   create("SObject",
+ *     field("FirstName", sourceValue("$.firstName"))
+ *   )
+ * )
+ * @constructor
+ * @param {string} path - JSONPath referencing a point in `state.data`.
+ * @param {function} operation - The operation needed to be repeated.
+ * @param {State} state - Runtime state.
+ * @returns {<State>}
+ */
+export const map = curry(function(path, operation, state) {
+
+  switch (typeof path) {
+    case 'string':
+      source(path)(state).map(function(data) {
+        return operation({data, references: state.references});
+      });      
+      return state
+
+    case 'object':
+      path.map(function(data) {
+        return operation({data, references: state.references});
+      });      
+      return state
+
+  }
+});
+
+/**
+ * Simple switcher allowing other expressions to use either a JSONPath or
+ * object literals as a data source.
+ * @constructor
+ * @param {string|object|function} data 
+ * - JSONPath referencing a point in `state`
+ * - Object Literal of the data itself.
+ * - Function to be called with state.
+ * @param {object} state - The current state.
+ * @returns {array}
+ */
+function asData(data, state) {
+  switch (typeof data) {
+    case 'string':
+      return source(data)(state)
+    case 'object':
+      return data
+    case 'function':
+      return data(state)
+  }        
+}
+
+/**
+ * Scopes an array of data based on a JSONPath.
+ * Useful when the source data has `n` items you would like to map to
+ * an operation.
+ * The operation will receive a slice of the data based of each item
+ * of the JSONPath provided.
+ *
+ * It also ensures the results of an operation make their way back into
+ * the state's references.
+ *
+ * @example <caption>Simple Example</caption>
+ * each("$.[*]",
+ *   create("SObject",
+ *     field("FirstName", sourceValue("$.firstName"))
+ *   )
+ * )
+ * @constructor
+ * @param {string} path - JSONPath referencing a point in `state`.
+ * @param {function} operation - The operation needed to be repeated.
+ * @returns {<Operation>}
+ */
+export function each(path, operation) {
+  if (!path) {
+    throw new TypeError("path argument for each expression is invalid.")
+  }
+
+  return (state) => {
+    return asData(path,state).reduce(function(state, data) {
+      if (state.then) {
+        return state.then((state) => {
+          return operation({ ...state, data })
+        })
+      } else {
+        return operation({ ...state, data })
+      }
+    }, state)
+
+  }
+}
+
+/**
+ * Combines two operations into one
+ * @constructor
+ * @param {...operations} operations - Any unfufilled operation.
+ * @returns {<Operation>}
+ */
+export function combine(...operations) {
+  return (state) => {
+    return operations.reduce((state,operation) => {
+      if (state.then) {
+        return state.then((state) => {
+          return { ...state, ...operation(state) }
+        })
+      } else {
+        return { ...state, ...operation(state) }
+      }
+    }, state)
+  }
+}
+
+export function join(targetPath, sourcePath, targetKey) {
+  return (state) => {
+    return source(targetPath)(state).map((i) => {
+      return { [targetKey]: sourceValue(sourcePath, state), ...i }
+    })
+  }
+}
+
--- test/index.js
+++ test/index.js
@@ -1,6 +1,9 @@
 import { expect } from 'chai';
+import testData from './testData';
 
-import { execute } from '../src/index'
+import {
+  execute, each, join, source, sourceValue, map, combine
+} from '../src';
 
 describe("execute", () => {
 
@@ -28,5 +31,126 @@ describe("execute", () => {
 
     expect(finalState).to.eql(state)
   
+  })
+})
+
+describe("sourceValue", () => {
+  it("references a given path", () => {
+    let value = sourceValue("$.store.bicycle.color", testData);
+    expect(value).to.eql("red");
+  })
+})
+
+describe("source", () => {
+  it("references a given path", () => {
+    let value = source("$.store.bicycle.color")(testData);
+    expect(value).to.eql([ "red" ]);
+  })
+})
+
+describe("each", () => {
+
+  let state
+  let operation = ({data, references}) => { return {
+    data, references: [data, ...references]
+  } }
+
+  beforeEach(function() {
+    state = { data: testData, references: [] }
+  })
+
+  it("maps the operation results in the references", () => {
+    let results = each( "$.data.store.book[*]", operation )(state)
+    expect(results.references.reverse()).to.eql(testData.store.book)
+  })
+
+  it("maps the operation results in the data", () => {
+    let results = each( testData.store.book, operation )(state)
+    expect(results.references.reverse()).to.eql(testData.store.book)
+  })
+
+  it("calls the data sourcing function", () => {
+    let sourcingFunction = (state) => {
+      return state.data.store.book
+    }
+
+    let results = each( sourcingFunction , operation )(state)
+    expect(results.references.reverse()).to.eql(testData.store.book)
+  })
+
+  it("resolves promise operations", (done) => {
+    let sourcingFunction = (state) => {
+      return state.data.store.book
+    }
+
+    operation = ({data, references}) => {
+      return Promise.resolve({
+        data, references: [data, ...references]
+      })
+    }
+
+    each( sourcingFunction , operation )(state).then((state) => {
+      expect(state.references.reverse()).to.eql(testData.store.book)
+    }).then(done).catch(done)
+  })
+
+})
+
+describe("map", () => {
+
+  xit("[DEPRECATED] can produce a one to one from an array", () => {
+    let items = [];
+
+    let state = { data: testData, references: [] }
+    let results = map('$.data.store.book[*]', 
+                      function(state) {
+                        console.log("hello");
+                        // console.log(JSON.stringify( state ));
+                        // items.push( { title: sourceValue("$.data.title", state) } )
+                        return { references: [1, ...state.references], ...state }
+                      }, state)
+
+                      expect(results.references).to.eql([
+                        { "title": "Sayings of the Century" },
+                        { "title": "Sword of Honour" },
+                        { "title": "Moby Dick" },
+                        { "title": "The Lord of the Rings" }
+                      ])
+
+  });
+
+})
+
+describe("combine", () => {
+
+  let state = { }
+  let operations = [
+    (state) => { return { hello: 1 } },
+      (state) => { return { hello: state.hello + 5 } }
+  ]
+
+
+  it("accepts serveral operations, and reduces them with the state", () => {
+    let result = combine.apply(null,operations)(state)
+    expect(result).to.eql({ hello: 6 })
+  })
+
+})
+
+describe("join", () => {
+  it("merges in a previously defined field", () => {
+    let result = join(
+      "$.store.book[*]",
+      "$.store.bicycle.color",
+      "color"
+    )(testData)
+
+    expect(result[0]).to.eql( {
+      "author": "Nigel Rees",
+      "category": "reference",
+      "color": "red",
+      "price": 8.95,
+      "title": "Sayings of the Century"
+    })
   })
 })

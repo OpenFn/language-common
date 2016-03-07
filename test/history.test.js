c1e4ce25af64f76ec7534ed70260973f88056bb7
Stuart Corbishley <corbish@gmail.com>
2016-03-07 11:48:02 +0200

    Catch errors

--- test/history.test.js
+++ test/history.test.js
@@ -4,32 +4,81 @@ import Utils from '../src/utils';
 
 describe("history", () => {
 
-  let state;
+  let initialState = {}
 
   function wrapOperation(operation) {
     return function(...operands) {
       return (state) => {
         // Finalize operands
         const finalOperands = Utils.finalizeOperands(...operands)(state)
-        // Fufill the operation with the operands.
-        const newState = operation(...finalOperands)(state)
 
-        // Merge in the result of this operation.
-        return {
-          ...newState,
-          // Append to existing history list, or add it to a new list
-          history: [ ...newState.history || [], 
-            {
-              type: 'operation',
-              name: operation.name,
-              status: 0,
-              finalOperands
-            }
-          ]
+        // Fufill the operation with the operands.
+        try {
+          const newState = operation(...finalOperands)(state)
+          // Merge in the result of this operation.
+          return {
+            ...newState,
+            // Append to existing history list, or add it to a new list
+            history: [ ...newState.history || [], 
+              {
+                type: 'operation',
+                name: operation.name,
+                status: 0,
+                finalOperands
+              }
+            ]
+          }
+        } catch (e) {
+          // Merge in the result of this operation.
+          return {
+            ...state,
+            // Append to existing history list, or add it to a new list
+            history: [ ...state.history || [], 
+              {
+                type: 'operation',
+                name: operation.name,
+                status: 1,
+                finalOperands,
+                error: e.toString()
+              }
+            ]
+          }
+          
         }
+
       }
     }
   }
+
+  it('catches errors and attaches them to history', function() {
+
+    function errorOperation(arg1, arg2) {
+      return state => {
+        throw new Error('An error has occured.')
+      }
+    }
+
+    let operation = wrapOperation(errorOperation)
+
+    return execute(
+      operation("arg1", "arg2")
+    )({}).then( (state) => {
+
+      expect(state).to.eql({ history: [
+
+        {
+          type: 'operation',
+          name: "errorOperation",
+          status: 1, 
+          finalOperands: ["arg1", "arg2"],
+          error: "Error: An error has occured."
+        }
+
+      ] })
+
+    })
+
+  })
 
   it('wraps existing functions with meta data', function() {
 

26e87e4c4d3e464e2753b2613c14272f6e8d4a4b
Elias W. BA <Elias W. BA>
2021-09-30 14:08:50 +0000

    Updated the functions to add the unicode  in the regex. That unicode represent the char that's causing us problems

--- src/Adaptor.js
+++ src/Adaptor.js
@@ -477,6 +477,6 @@ export function splitKeys(obj, keys) {
 
 export function removeEmojis(text) {
   if (!text) return '';
-  const emojisPattern = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
+  const emojisPattern = /(\uFE0F|\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g;
   return text.replace(emojisPattern, '');
 }
